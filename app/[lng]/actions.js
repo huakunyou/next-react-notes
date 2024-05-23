9e50a05487e3ebfe05a304bc9b86cd8cba007934
Frank You <2464280815@qq.com>
2024-05-23 13:42:35 +0800

    upload md file use Server Action

--- app/[lng]/actions.js
+++ app/[lng]/actions.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : frank
  * @Date         : 2024-04-26 21:17:03
- * @LastEditTime : 2024-04-27 11:18:57
+ * @LastEditTime : 2024-05-23 10:51:52
  * @LastEditors  : frank
  * @Description  : In User Settings Edit
  */
@@ -12,6 +12,11 @@ import { addNote, updateNote, delNote } from '@/lib/redis';
 import { revalidatePath } from 'next/cache';
 import { z } from "zod";
 import { sleep } from '@/lib/utils'
+import { stat, mkdir, writeFile } from 'fs/promises'
+import { join } from "path";
+import mime from "mime";
+import dayjs from 'dayjs';
+
 
 const schema = z.object({
   title: z.string(),
@@ -59,6 +64,51 @@ export async function deleteNote(prevState, formData) {
 }
 
 
+export async function importNote(formData) {
+  const file = formData.get('file')
 
+  // 空值判断
+  if (!file) {
+    return { error: "File is required." };
+  }
+
+  // 写入文件
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const relativeUploadDir = `/uploads/${dayjs().format("YY-MM-DD")}`;
+  const uploadDir = join(process.cwd(), "public", relativeUploadDir);
+
+  try {
+    await stat(uploadDir);
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      await mkdir(uploadDir, { recursive: true });
+    } else {
+      console.error(e)
+      return { error: "Something went wrong." }
+    }
+  }
+
+  try {
+    // 写入文件
+    const uniqueSuffix = `${Math.random().toString(36).slice(-6)}`;
+    const filename = file.name.replace(/\.[^/.]+$/, "")
+    const uniqueFilename = `${filename}-${uniqueSuffix}.${mime.getExtension(file.type)}`;
+    await writeFile(`${uploadDir}/${uniqueFilename}`, buffer);
+
+    // 调用接口，写入数据库
+    const res = await addNote(JSON.stringify({
+      title: filename,
+      content: buffer.toString('utf-8')
+    }))
+
+    // 清除缓存
+    revalidatePath('/', 'layout')
+
+    return { fileUrl: `${relativeUploadDir}/${uniqueFilename}`, uid: res }
+  } catch (e) {
+    console.error(e)
+    return { error: "Something went wrong." }
+  }
+}
 
 
